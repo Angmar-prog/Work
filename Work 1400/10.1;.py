996e93b82c51ec6f646cda4561e8e51f95d80625
Angmar-prog <angmarrider555@gmail.com>
2025-04-05 11:57:16 +0500

    Add files via upload

--- Work 1400/10.1;.py
+++ Work 1400/10.1;.py
@@ -1,9 +1,10 @@
 import random
 
+a = int(input("Введите значение a: "))
 k = int(input("Введите значение k: "))
 b = int(input("Введите значение b: "))
 if k > b:
     k, b = b, k
-for _ in range(k, b):
-    random_float = random.uniform(k, b)
+for _ in range(a):  
+    random_float = random.uniform(k, b)  
     print(random_float)
---
+++ Work 1400/10.1e.py
@@ -0,0 +1,5 @@
+import random
+
+for _ in range(10):
+    random_float = random.uniform(-40, 40)
+    print(random_float)
