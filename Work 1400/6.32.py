aee1f6583c31d6e272031eef19932a34ba1332f8
Angmar-prog <angmarrider555@gmail.com>
2025-04-24 16:19:45 +0500

    Add files via upload

--- Work 1400/6.32.py
+++ Work 1400/6.32.py
@@ -1,5 +1,9 @@
-kmh = 10 # каждый день
-ms = 10 # процент
- 
-if kmh<ms * 1000/3600:
-    print(f"{ms} м/с больше, чем {kmh} кл/час")
+
+id = 10
+ip = 0.10
+d = 10
+cd = id
+print("День\tПробег (км)")
+for da in range(1, d + 1):
+    print(f"{da}\t{cd:.2f}")
+    cd += cd * ip
